ef5ced163a36945446b7b2c36c56415b1a31361e
Denzel Kanyeki <denzelkinyua11@gmail.com>
2024-02-29 22:32:20 +0300

    Second commit

--- mail.py
+++ mail.py
@@ -1,25 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_mail import Mail, Message
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 mail = Mail(app)
 
-MAIL_SERVER = 'smtp.gmail.com'
-MAIL_PORT = 465
-MAIL_USE_SSL = True
+app.config["MAIL_SERVER"] = 'smtp.gmail.com'
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "denzelkinyua11@gmail.com"
+app.config["MAIL_PASSWORD"] = os.getenv('EMAIL_PASS')
 
 
-load_dotenv()
+@app.route("/", methods=["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        msg = Message("Hello", sender="demo@no-reply.com", recipients=['denzelkinyua11@gmail.com'])
+        msg.body = "Hello, this is a test email."
+        mail.send(msg)
+        return "Email sent successfully!"
+    return render_template('index.html')
 
-@app.route('/', methods=['POST', 'GET'])
-def send_mail():
-    msg = Message('Hello, this is a test program to send out emails', sender='demo@company.com', recipients=[os.getenv('RECIPIENT_EMAIL')])
-    mail.send(msg)
 
-
-
-if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
