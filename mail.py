42b01eec6d1c0cf947e68a6502f2b5f0676fbe4c
Denzel Kanyeki <denzelkinyua11@gmail.com>
2024-03-01 11:16:22 +0300

    Minor change

--- mail.py
+++ mail.py
@@ -8,11 +8,11 @@ load_dotenv()
 app = Flask(__name__)
 mail = Mail(app)
 
-app.config["MAIL_SERVER"] = 'smtp.gmail.com'
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_TLS"] = False
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = "denzelkinyua11@gmail.com"
+app.config["MAIL_SERVER"] = 'smtp.office365.com'
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_USERNAME"] = os.getenv("EMAIL_USER")
 app.config["MAIL_PASSWORD"] = os.getenv('EMAIL_PASS')
 
 
@@ -20,7 +20,7 @@ app.config["MAIL_PASSWORD"] = os.getenv('EMAIL_PASS')
 @app.route("/home", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        msg = Message("Hello", sender="demo@no-reply.com", recipients=['denzelkinyua11@gmail.com'])
+        msg = Message("Hello", sender="demo@no-reply.com", recipients=['denzoooo@outlook.com'])
         msg.body = "Hello, this is a test email."
         mail.send(msg)
         return "Email sent successfully!"
